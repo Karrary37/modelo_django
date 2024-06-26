a40993f1d490c181616629cb32af6928f2f07e99
Nattan Karrary <74218114+Karrary37@users.noreply.github.com>
2024-06-26 10:56:13 -0300

    Pre-commit negativo.

--- core/views.py
+++ core/views.py
@@ -2,4 +2,5 @@ from django.http import HttpResponseRedirect
 
 
 def index(request):
+    print('-----------------------------TESTE PRE-COMMIT-------------------------------- ')
     return HttpResponseRedirect('/backoffice/')
